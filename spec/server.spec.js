94159dbdf270a63565ffa3d17d6cf951644dc6fb
Lee Len Formento <leebatef@gmail.com>
2018-11-13 23:49:06 -0500

    more fixes

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 describe('get messages', () => {
     it('should return 200 Ok', (done) => {
-        request.get('http://localhost:4000/messages', (err, res) => {
+        request.get('https://lee-chat.herokuapp.com/messages', (err, res) => {
             expect(res.statusCode).toEqual(200)
             done()
         })
     })
     it('should return a list that is not empty', (done) => {
-        request.get('http://localhost:4000/messages', (err, res) => {
+        request.get('https://lee-chat.herokuapp.com/messages', (err, res) => {
             expect(JSON.parse(res.body)[0].name).toEqual('tim')
             done()
         })
